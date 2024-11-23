e36121e458652c3b75e83d1d7a25f3989d5619ff
Nikione7 <grandioznovnikilandzello@gmail.com>
2024-11-23 16:41:56 +0300

    Add files via upload

--- homework21.py
+++ homework21.py
@@ -1,6 +1,6 @@
-Number_of_completed_tasks = 12
-Number_of_hours_spent = 1.5
-Course_name = 'Python'
-Time_for_one_task = Number_of_hours_spent / Number_of_completed_tasks
-print(Time_for_one_task)
-print('Курс:',Course_name,',','всего задач:', Number_of_completed_tasks, ',','затрачено часов:', Number_of_hours_spent, ',','среднее время выполнения', Time_for_one_task, 'часа.')
+example = 'pencil'
+print(example[0])
+print(example[-1])
+print(example[3:])
+print(example[::-1])
+print(example[1::2])
